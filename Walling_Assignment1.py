9af5bbf7ef5880fd73df5ca7592f6d321f4d2429
Alex Walling <alexwalling@Alexs-MacBook-Pro-2.local>
2015-08-31 00:10:10 -0600

    Commit 3

--- Walling_Assignment1.py
+++ Walling_Assignment1.py
@@ -1,2 +1,23 @@
+class Queue:
+	def __init__(self):
+		self.items = []
 
-hello
+	def isEmpty(self):
+		return self.items == {}
+
+	def enqueue(self, item):
+		self.items.insert(0,item)
+
+	def dequeue(self):
+		return self.items.pop()
+
+	def size(self):
+		return len(self.items)
+
+q=Queue()
+
+q.enqueue(4)
+q.enqueue('dog')
+q.enqueue(True)
+print(q.size())
+
